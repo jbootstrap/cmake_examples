644a0379737443cb7134b0bbd2c41259571fdd0e
root <root@root-pc>
2016-07-12 18:09:54 +0900

    .

--- opencv_sample/cvwin.cpp
+++ opencv_sample/cvwin.cpp
@@ -11,20 +11,70 @@ HBITMAP cvwin::IconToBitmap(HICON hIcon)
 	ICONINFO iconInfo;
 	GetIconInfo(hIcon, &iconInfo);
 	GetObject(iconInfo.hbmColor, sizeof(BITMAP), &bm);
-	int width = bm.bmWidth;
-	int height = bm.bmHeight;
 	HDC hDC = GetDC(NULL);
 	HDC hMemDC = CreateCompatibleDC(hDC);
-	HBITMAP hMemBmp = CreateCompatibleBitmap(hDC, width, height);
+	HBITMAP hMemBmp = CreateCompatibleBitmap(hDC, bm.bmWidth, bm.bmHeight);
 	HBITMAP hResultBmp = NULL;
 	HGDIOBJ hOrgBMP = SelectObject(hMemDC, hMemBmp);
-	DrawIconEx(hMemDC, 0, 0, hIcon, width, height, 0, NULL, DI_NORMAL);
+	DrawIconEx(hMemDC, 0, 0, hIcon, bm.bmWidth, bm.bmHeight, 0, NULL, DI_NORMAL);
 	hResultBmp = hMemBmp;
 	hMemBmp = NULL;
 	SelectObject(hMemDC, hOrgBMP);
 	DeleteDC(hMemDC);
 	ReleaseDC(NULL, hDC);
 	return hResultBmp;
+}
+
+std::vector<uchar> cvwin::BitmapToPng(HBITMAP hBitmap)
+{
+	std::vector<uchar> png_data;
+	CImage cimg;
+	cimg.Attach(hBitmap);
+	IStream* pIStream = NULL;
+	if (CreateStreamOnHGlobal(NULL, TRUE, (LPSTREAM*)&pIStream) != S_OK)
+	{
+		cimg.Detach();
+		DeleteObject(hBitmap);
+		return png_data;
+	}
+	if (cimg.Save(pIStream, Gdiplus::ImageFormatPNG) != S_OK)
+	{
+		pIStream->Release();
+		cimg.Detach();
+		DeleteObject(hBitmap);
+		return png_data;
+	}
+	cimg.Detach();
+	DeleteObject(hBitmap);
+	ULARGE_INTEGER ulnSize;
+	LARGE_INTEGER lnOffset;
+	lnOffset.QuadPart = 0;
+	if (pIStream->Seek(lnOffset, STREAM_SEEK_END, &ulnSize) != S_OK)
+	{
+		pIStream->Release();
+		return png_data;
+	}
+	if (pIStream->Seek(lnOffset, STREAM_SEEK_SET, NULL) != S_OK)
+	{
+		pIStream->Release();
+		return png_data;
+	}
+	png_data.resize((unsigned int)ulnSize.QuadPart);
+	ULONG ulBytesRead;
+	if (pIStream->Read(&png_data[0], (ULONG)ulnSize.QuadPart, &ulBytesRead) != S_OK)
+	{
+		pIStream->Release();
+		return png_data;
+	}
+	pIStream->Release();
+	return png_data;
+}
+
+cv::Mat cvwin::BitmapToMat(HBITMAP hBitmap)
+{
+	std::vector<uchar> png = cvwin::BitmapToPng(hBitmap);
+	cv::Mat mat = cv::imdecode(cv::Mat(png), 1);
+	return mat;
 }
 
 std::vector<uchar> cvwin::IconToPng(HICON hIcon)
@@ -37,58 +87,8 @@ std::vector<uchar> cvwin::IconToPng(HICON hIcon)
 
 cv::Mat cvwin::IconToMat(HICON hIcon)
 {
-	std::vector<uchar> png = cvwin::IconToPng(hIcon);
-	cv::Mat mat = cv::imdecode(cv::Mat(png), 1);
-	return mat;
-}
-
-std::vector<uchar> cvwin::BitmapToPng(HBITMAP hBitmap)
-{
-	std::vector<uchar> buff;
-	CImage cimg;
-	cimg.Attach(hBitmap);
-	IStream* pIStream = NULL;
-	if (CreateStreamOnHGlobal(NULL, TRUE, (LPSTREAM*)&pIStream) != S_OK)
-	{
-		DeleteObject(hBitmap);
-		return buff;
-	}
-	HRESULT SaveStatus = cimg.Save(pIStream, Gdiplus::ImageFormatPNG);
-	if (SaveStatus != S_OK)
-	{
-		pIStream->Release();
-		DeleteObject(hBitmap);
-		return buff;
-	}
-	cimg.Detach();
+	HBITMAP hBitmap = cvwin::IconToBitmap(hIcon);
+	cv::Mat mat = cvwin::BitmapToMat(hBitmap);
 	DeleteObject(hBitmap);
-	ULARGE_INTEGER ulnSize;
-	LARGE_INTEGER lnOffset;
-	lnOffset.QuadPart = 0;
-	if (pIStream->Seek(lnOffset, STREAM_SEEK_END, &ulnSize) != S_OK)
-	{
-		pIStream->Release();
-		return buff;
-	}
-	if (pIStream->Seek(lnOffset, STREAM_SEEK_SET, NULL) != S_OK)
-	{
-		pIStream->Release();
-		return buff;
-	}
-	buff.resize((unsigned int)ulnSize.QuadPart);
-	ULONG ulBytesRead;
-	if (pIStream->Read(&buff[0], (ULONG)ulnSize.QuadPart, &ulBytesRead) != S_OK)
-	{
-		pIStream->Release();
-		return buff;
-	}
-	pIStream->Release();
-	return buff;
-}
-
-cv::Mat cvwin::BitmapToMat(HBITMAP hBitmap)
-{
-	std::vector<uchar> png = cvwin::BitmapToPng(hBitmap);
-	cv::Mat mat = cv::imdecode(cv::Mat(png), 1);
 	return mat;
 }
--- opencv_sample/cvwin.h
+++ opencv_sample/cvwin.h
@@ -9,10 +9,10 @@ namespace cvwin {
 
 	HBITMAP IconToBitmap(HICON hIcon);
 
-	std::vector<uchar> IconToPng(HICON hIcon);
-	cv::Mat IconToMat(HICON hIcon);
-
 	std::vector<uchar> BitmapToPng(HBITMAP hBitmap);
 	cv::Mat BitmapToMat(HBITMAP hBitmap);
+
+	std::vector<uchar> IconToPng(HICON hIcon);
+	cv::Mat IconToMat(HICON hIcon);
 
 }
